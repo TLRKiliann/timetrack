2a3a5d2dcbd367a44bd5460432b24c96336a3c66
TLRKiliann <philogenie@protonmail.com>
2021-10-21 17:35:01 +0200

    matplotlib improved

--- param/aspifile7/plot/plot_sat.py
+++ param/aspifile7/plot/plot_sat.py
@@ -52,12 +52,12 @@ print(list2)
 try:
     list1 = list(map(str, list1))
 except ValueError as fmt_err:
-    print("+ Invalid number (no: . or , !)", fmt_err)
+    print("[!] Invalid number (no: . or , !)", fmt_err)
 
 try:
     list2 = list(map(int, list2))
 except ValueError as base_err:
-    print("+ Invalid number (no: . or , !)", base_err)
+    print("[!] Invalid number (no: . or , !)", base_err)
     list2 = []
 
 xdates = [datetime.datetime.strptime('{:10}'.format(str(li)),'%d/%m/%Y : %H:%M:%S') for li in list1]
@@ -69,43 +69,42 @@ y_axis = list2
 try:
     show_grid = True
     with plt.style.context('seaborn-darkgrid'):
-        #figure, axes = plt.subplots()
         fig = plt.figure()
         fig.set_facecolor('lightsteelblue')
-        lab = fig.suptitle('SaO2 (%) by Day',
+        lab = fig.suptitle('Oxygen Saturation (SaO2%)',
             fontsize=18)
         lab.set_color('navy')
         ax = plt.subplot()
         ax.tick_params(axis='x', colors='navy')
         ax.tick_params(axis='y', colors='navy')
-        labelc = plt.ylabel("y-label")
-        labelc.set_color('navy')
-        labelc2 = plt.xlabel("x-label")
-        labelc2.set_color('navy')
-        plt.plot(x_axis, y_axis, 'o', color='blue')
-        plt.plot(x_axis, y_axis, '--', color='blue')
+        labelcol_y = plt.ylabel("y-label")
+        labelcol_y.set_color('navy')
+        labelcol_x = plt.xlabel("x-label")
+        labelcol_x.set_color('navy')
         
         for x,y in zip(x_axis, y_axis):
             label = "{}".format(y)
             plt.annotate(label, (x,y), textcoords="offset points",
                 xytext=(0,10), ha='center')
 
+        reo_x, reo_y = zip(*sorted(zip(x_axis, y_axis)))
+        plt.plot(reo_x, reo_y, 'o', color='teal')
+        plt.plot(reo_x, reo_y, '--', color='teal')
+
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y : %H:%M:%S'))
-        plt.ylabel('SaO2', fontsize=14)
+        plt.ylabel('Oxygen Saturation (SaO2%)', fontsize=14)
         plt.xlabel('Dates', fontsize=14)
-        #plt.title('Relevé des SaO2 en % par date', fontsize=16)
-        #plt.xticks(rotation=25)
-        plt.legend(['SaO2 %'])
+        plt.legend(['SaO2%'])
         plt.gcf().autofmt_xdate(rotation=25)
         plt.grid(show_grid)
         plt.show()
-except ValueError as shapes_err:
-    print("Invalid number", shapes_err)
+except Exception as err_val:
+    print("[!] Value Error !!! :", err_val)
 
 try:
     os.remove('./param/aspifile7/data_datesat.json')
-    print("+ File data_datesat.json removed !")
+    print("[+] File data_datesat.json removed !")
     os.remove('./param/aspifile7/data_sat.json')
-    print("+ File data_sat.json removed !\n")
+    print("[+] File data_sat.json removed !\n")
 except OSError as os_err:
-    print("+ OS error ! ...", os_err)
+    print("[!] OS error ! ...", os_err)
