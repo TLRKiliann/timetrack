263ab9167e3906dc97f579a9a0e1ebcedc9606e9
TLRKiliann <philogenie@protonmail.com>
2021-10-21 15:45:53 +0200

    script improved

--- param/aspifile21/plot/plot_freq.py
+++ param/aspifile21/plot/plot_freq.py
@@ -52,12 +52,12 @@ print(list2)
 try:
     list1 = list(map(str, list1))
 except ValueError as fmt_err:
-    print("+ Invalid number (no: . or , !)", fmt_err)
+    print("[!] Invalid number (no: . or , !)", fmt_err)
 
 try:
     list2 = list(map(int, list2))
 except ValueError as base_err:
-    print("+ Invalid number (no: . or , !)", base_err)
+    print("[!] Invalid number (no: . or , !)", base_err)
     list2 = []
 
 xdates = [datetime.datetime.strptime('{:10}'.format(str(li)),'%d/%m/%Y : %H:%M:%S') for li in list1]
@@ -69,43 +69,42 @@ y_axis = list2
 try:
     show_grid = True
     with plt.style.context('seaborn-darkgrid'):
-        #figure, axes = plt.subplots()
         fig = plt.figure()
         fig.set_facecolor("lightsteelblue")
-        lab = fig.suptitle('FR/min by Day',
+        lab = fig.suptitle('Respiratory Frequency',
             fontsize=18)
         lab.set_color('navy')
         ax = plt.subplot()
         ax.tick_params(axis='x', colors='navy')
         ax.tick_params(axis='y', colors='navy')
-        labelc = plt.ylabel("y-label")
-        labelc.set_color('navy')
-        labelc2 = plt.xlabel("x-label")
-        labelc2.set_color('navy')
+        labelcol_y = plt.ylabel("y-label")
+        labelcol_y.set_color('navy')
+        labelcol_x = plt.xlabel("x-label")
+        labelcol_x.set_color('navy')
 
-        plt.plot(x_axis, y_axis, 's', color='blue')
-        plt.plot(x_axis, y_axis, '--', color='cyan')
         for x,y in zip(x_axis, y_axis):
             label = "{}".format(y)
             plt.annotate(label, (x,y), textcoords="offset points",
                 xytext=(0,10), ha='center')
 
+        reo_x, reo_y = zip(*sorted(zip(x_axis, y_axis)))
+        plt.plot(reo_x, reo_y, 'o', color='cyan')
+        plt.plot(reo_x, reo_y, '-', color='cyan')
+
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y : %H:%M:%S'))
-        plt.ylabel('FR/min', fontsize=14)
+        plt.ylabel('Resp/min', fontsize=14)
         plt.xlabel('Dates', fontsize=14)
-        #plt.title('Relevé des fréquences resp (FR/min) par date', fontsize=16)
-        #plt.xticks(rotation=25)
-        plt.legend(['Respiratory Frequency'])
+        plt.legend(['Resp/min'])
         plt.gcf().autofmt_xdate(rotation=25)
         plt.grid(show_grid)
         plt.show()
-except ValueError as shapes_err:
-    print("Invalid number", shapes_err)
+except Exception as err_val:
+    print("[!] Value Error !!! :", err_val)
 
 try:
     os.remove('./param/aspifile21/data_datefr.json')
-    print("+ File data_datefr.json removed !")
+    print("[+] File data_datefr.json removed !")
     os.remove('./param/aspifile21/data_fr.json')
-    print("+ File data_fr.json removed !\n")
+    print("[+] File data_fr.json removed !\n")
 except OSError as os_err:
-    print("+ OS error ! ...", os_err)
+    print("[!] OS error ! ...", os_err)
