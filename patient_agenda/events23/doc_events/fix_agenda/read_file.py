4903cd6d8a598359be501440d63b2f8d77fac4b4
TLRKiliann <philogenie@protonmail.com>
2021-10-26 16:25:10 +0200

    agenda on work

--- patient_agenda/events23/doc_events/fix_agenda/read_file.py
+++ patient_agenda/events23/doc_events/fix_agenda/read_file.py
@@ -2,44 +2,44 @@
 # -*- coding: utf-8 -*-
 
 
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 import os
 import posixpath
 
 
-fen=Tk()
+fen = tk.Tk()
 fen.title("Time-Track")
 fen.configure(background='DodgerBlue2')
+fen.resizable(False, False)
 
-# To place side by side labelo + entrylab
-top = Frame(fen, bg='DodgerBlue2')
-top2 = Frame(fen, bg='DodgerBlue2')
-top3 = Frame(fen, bg='SteelBlue2')
-top4 = Frame(fen, bg='SteelBlue2')
-top5 = Frame(fen, bg='DodgerBlue2')
-bottom = Frame(fen, bg='DodgerBlue2')
-top.pack(side=TOP)
-top2.pack(side=TOP)
-top3.pack(side=TOP)
-top4.pack(side=TOP)
-top5.pack(side=TOP)
-bottom.pack(side=BOTTOM, fill=BOTH, expand=YES)
+top = tk.Frame(fen, bg='DodgerBlue2')
+top2 = tk.Frame(fen, bg='DodgerBlue2')
+top3 = tk.Frame(fen, bg='SteelBlue2')
+top4 = tk.Frame(fen, bg='SteelBlue2')
+top5 = tk.Frame(fen, bg='DodgerBlue2')
+bottom = tk.Frame(fen, bg='DodgerBlue2')
+top.pack(side=tk.TOP)
+top2.pack(side=tk.TOP)
+top3.pack(side=tk.TOP)
+top4.pack(side=tk.TOP)
+top5.pack(side=tk.TOP)
+bottom.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=tk.YES)
 
-labelo = Label(fen, text="Appointments",
+labelo = tk.Label(fen, text="Appointments",
     font='Arial 18 bold', fg='white', bg='DodgerBlue2')
-labelo.pack(in_=top, side=LEFT, pady=5)
+labelo.pack(in_=top, side=tk.LEFT, pady=5)
 
 with open('./newpatient/entryfile23.txt', 'r') as filename:
     line1 = filename.readline()
 
-textentry = StringVar()
-entrylab = Entry(fen, textvariable=textentry, width=20, 
+textentry = tk.StringVar()
+entrylab = tk.Entry(fen, textvariable=textentry, width=20, 
     highlightbackground='grey', bd=3)
 textentry.set(line1[:-1])
-entrylab.pack(in_=top, side=RIGHT, padx=10, pady=10)
+entrylab.pack(in_=top, side=tk.RIGHT, padx=10, pady=10)
 
-textBox = Text(fen, height=20, width=60, font=18)
+textBox = tk.Text(fen, height=20, width=60, font=18)
 textBox.pack(in_=top2, padx=30, pady=10)
 
 def janSearch():
@@ -57,18 +57,18 @@ def janSearch():
                         line.replace('}', '')
                         line = lines[i]
                         if line[3:5] == "01":
-                            textBox.insert(INSERT, lines[i-1])
-                            textBox.insert(INSERT, lines[i])
-                            textBox.insert(INSERT, lines[i+1])
-                            textBox.insert(INSERT, lines[i+2])
-                            textBox.insert(INSERT, '\n')
+                            textBox.insert(tk.INSERT, lines[i-1])
+                            textBox.insert(tk.INSERT, lines[i])
+                            textBox.insert(tk.INSERT, lines[i+1])
+                            textBox.insert(tk.INSERT, lines[i+2])
+                            textBox.insert(tk.INSERT, '\n')
                             print(lines[i])
                             break
 
-buttonJan = Button(fen, text="Jan", font='Arial 12 bold', bd=3,
+buttonJan = tk.Button(fen, text="Jan", font='Arial 12 bold', bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
     activebackground='pale turquoise', command=janSearch)
-buttonJan.pack(in_=top3, side=LEFT, padx=10, pady=10)
+buttonJan.pack(in_=top3, side=tk.LEFT, padx=10, pady=10)
 
 def febSearch():
     for path, dirs, files in os.walk('./patient_agenda/'\
@@ -82,18 +82,17 @@ def febSearch():
                         line.replace('}', '')
                         line = lines[i]
                         if line[3:5] == "02":
-                            textBox.insert(INSERT, lines[i-1])
-                            textBox.insert(INSERT, lines[i])
-                            textBox.insert(INSERT, lines[i+1])
-                            textBox.insert(INSERT, lines[i+2])
-                            textBox.insert(INSERT, '\n')
+                            textBox.insert(tk.INSERT, lines[i-1])
+                            textBox.insert(tk.INSERT, lines[i])
+                            textBox.insert(tk.INSERT, lines[i+1])
+                            textBox.insert(tk.INSERT, lines[i+2])
+                            textBox.insert(tk.INSERT, '\n')
                             break
 
-buttonFeb = Button(fen, text="Feb", font='Arial 12 bold', bd=3,
+buttonFeb = tk.Button(fen, text="Feb", font='Arial 12 bold', bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
-    activebackground='pale turquoise',
-    command=febSearch)
-buttonFeb.pack(in_=top3, side=LEFT, padx=10, pady=10)
+    activebackground='pale turquoise', command=febSearch)
+buttonFeb.pack(in_=top3, side=tk.LEFT, padx=10, pady=10)
 
 def marSearch():
     for path, dirs, files in os.walk('./patient_agenda/'\
@@ -107,18 +106,17 @@ def marSearch():
                         line.replace('}', '')
                         line = lines[i]
                         if line[3:5] == "03":
-                            textBox.insert(INSERT, lines[i-1])
-                            textBox.insert(INSERT, lines[i])
-                            textBox.insert(INSERT, lines[i+1])
-                            textBox.insert(INSERT, lines[i+2])
-                            textBox.insert(INSERT, '\n')
+                            textBox.insert(tk.INSERT, lines[i-1])
+                            textBox.insert(tk.INSERT, lines[i])
+                            textBox.insert(tk.INSERT, lines[i+1])
+                            textBox.insert(tk.INSERT, lines[i+2])
+                            textBox.insert(tk.INSERT, '\n')
                             break
 
-buttonMar = Button(fen, text="Mar", font='Arial 12 bold', bd=3,
+buttonMar = tk.Button(fen, text="Mar", font='Arial 12 bold', bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
-    activebackground='pale turquoise',
-    command=marSearch)
-buttonMar.pack(in_=top3, side=LEFT, padx=10, pady=10)
+    activebackground='pale turquoise', command=marSearch)
+buttonMar.pack(in_=top3, side=tk.LEFT, padx=10, pady=10)
 
 def avrSearch():
     for path, dirs, files in os.walk('./patient_agenda/'\
@@ -132,18 +130,17 @@ def avrSearch():
                         line.replace('}', '')
                         line = lines[i]
                         if line[3:5] == "04":
-                            textBox.insert(INSERT, lines[i-1])
-                            textBox.insert(INSERT, lines[i])
-                            textBox.insert(INSERT, lines[i+1])
-                            textBox.insert(INSERT, lines[i+2])
-                            textBox.insert(INSERT, '\n')
+                            textBox.insert(tk.INSERT, lines[i-1])
+                            textBox.insert(tk.INSERT, lines[i])
+                            textBox.insert(tk.INSERT, lines[i+1])
+                            textBox.insert(tk.INSERT, lines[i+2])
+                            textBox.insert(tk.INSERT, '\n')
                             break
 
-buttonAvr = Button(fen, text="Avr", font='Arial 12 bold', bd=3,
+buttonAvr = tk.Button(fen, text="Avr", font='Arial 12 bold', bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
-    activebackground='pale turquoise',
-    command=avrSearch)
-buttonAvr.pack(in_=top3, side=LEFT, padx=10, pady=10)
+    activebackground='pale turquoise', command=avrSearch)
+buttonAvr.pack(in_=top3, side=tk.LEFT, padx=10, pady=10)
 
 def maiSearch():
     for path, dirs, files in os.walk('./patient_agenda/'\
@@ -157,18 +154,17 @@ def maiSearch():
                         line.replace('}', '')
                         line = lines[i]
                         if line[3:5] == "05":
-                            textBox.insert(INSERT, lines[i-1])
-                            textBox.insert(INSERT, lines[i])
-                            textBox.insert(INSERT, lines[i+1])
-                            textBox.insert(INSERT, lines[i+2])
-                            textBox.insert(INSERT, '\n')
+                            textBox.insert(tk.INSERT, lines[i-1])
+                            textBox.insert(tk.INSERT, lines[i])
+                            textBox.insert(tk.INSERT, lines[i+1])
+                            textBox.insert(tk.INSERT, lines[i+2])
+                            textBox.insert(tk.INSERT, '\n')
                             break
 
-buttonMay = Button(fen, text="May", font='Arial 12 bold', bd=3,
+buttonMay = tk.Button(fen, text="May", font='Arial 12 bold', bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
-    activebackground='pale turquoise',
-    command=maiSearch)
-buttonMay.pack(in_=top3, side=LEFT, padx=10, pady=10)
+    activebackground='pale turquoise', command=maiSearch)
+buttonMay.pack(in_=top3, side=tk.LEFT, padx=10, pady=10)
 
 def junSearch():
     for path, dirs, files in os.walk('./patient_agenda/'\
@@ -182,18 +178,17 @@ def junSearch():
                         line.replace('}', '')
                         line = lines[i]
                         if line[3:5] == "06":
-                            textBox.insert(INSERT, lines[i-1])
-                            textBox.insert(INSERT, lines[i])
-                            textBox.insert(INSERT, lines[i+1])
-                            textBox.insert(INSERT, lines[i+2])
-                            textBox.insert(INSERT, '\n')
+                            textBox.insert(tk.INSERT, lines[i-1])
+                            textBox.insert(tk.INSERT, lines[i])
+                            textBox.insert(tk.INSERT, lines[i+1])
+                            textBox.insert(tk.INSERT, lines[i+2])
+                            textBox.insert(tk.INSERT, '\n')
                             break
 
-buttonJun = Button(fen, text="Jun", font='Arial 12 bold', bd=3,
+buttonJun = tk.Button(fen, text="Jun", font='Arial 12 bold', bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
-    activebackground='pale turquoise',
-    command=junSearch)
-buttonJun.pack(in_=top3, side=LEFT, padx=10, pady=10)
+    activebackground='pale turquoise', command=junSearch)
+buttonJun.pack(in_=top3, side=tk.LEFT, padx=10, pady=10)
 
 def julSearch():
     for path, dirs, files in os.walk('./patient_agenda/'\
@@ -207,18 +202,17 @@ def julSearch():
                         line.replace('}', '')
                         line = lines[i]
                         if line[3:5] == "07":
-                            textBox.insert(INSERT, lines[i-1])
-                            textBox.insert(INSERT, lines[i])
-                            textBox.insert(INSERT, lines[i+1])
-                            textBox.insert(INSERT, lines[i+2])
-                            textBox.insert(INSERT, '\n')
+                            textBox.insert(tk.INSERT, lines[i-1])
+                            textBox.insert(tk.INSERT, lines[i])
+                            textBox.insert(tk.INSERT, lines[i+1])
+                            textBox.insert(tk.INSERT, lines[i+2])
+                            textBox.insert(tk.INSERT, '\n')
                             break
 
-buttonJul = Button(fen, text="Jul", font='Arial 12 bold', bd=3,
+buttonJul = tk.Button(fen, text="Jul", font='Arial 12 bold', bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
-    activebackground='pale turquoise',
-    command=julSearch)
-buttonJul.pack(in_=top4, side=LEFT, padx=10, pady=10)
+    activebackground='pale turquoise', command=julSearch)
+buttonJul.pack(in_=top4, side=tk.LEFT, padx=10, pady=10)
 
 def augSearch():
     for path, dirs, files in os.walk('./patient_agenda/'\
@@ -232,18 +226,17 @@ def augSearch():
                         line.replace('}', '')
                         line = lines[i]
                         if line[3:5] == "08":
-                            textBox.insert(INSERT, lines[i-1])
-                            textBox.insert(INSERT, lines[i])
-                            textBox.insert(INSERT, lines[i+1])
-                            textBox.insert(INSERT, lines[i+2])
-                            textBox.insert(INSERT, '\n')
+                            textBox.insert(tk.INSERT, lines[i-1])
+                            textBox.insert(tk.INSERT, lines[i])
+                            textBox.insert(tk.INSERT, lines[i+1])
+                            textBox.insert(tk.INSERT, lines[i+2])
+                            textBox.insert(tk.INSERT, '\n')
                             break
 
-buttonAug = Button(fen, text="Aug", font='Arial 12 bold', bd=3,
+buttonAug = tk.Button(fen, text="Aug", font='Arial 12 bold', bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
-    activebackground='pale turquoise',
-    command=augSearch)
-buttonAug.pack(in_=top4, side=LEFT, padx=10, pady=10)
+    activebackground='pale turquoise', command=augSearch)
+buttonAug.pack(in_=top4, side=tk.LEFT, padx=10, pady=10)
 
 def sepSearch():
     for path, dirs, files in os.walk('./patient_agenda/'\
@@ -257,18 +250,17 @@ def sepSearch():
                         line.replace('}', '')
                         line = lines[i]
                         if line[3:5] == "09":
-                            textBox.insert(INSERT, lines[i-1])
-                            textBox.insert(INSERT, lines[i])
-                            textBox.insert(INSERT, lines[i+1])
-                            textBox.insert(INSERT, lines[i+2])
-                            textBox.insert(INSERT, '\n')
+                            textBox.insert(tk.INSERT, lines[i-1])
+                            textBox.insert(tk.INSERT, lines[i])
+                            textBox.insert(tk.INSERT, lines[i+1])
+                            textBox.insert(tk.INSERT, lines[i+2])
+                            textBox.insert(tk.INSERT, '\n')
                             break
 
-buttonSep = Button(fen, text="Sep", font='Arial 12 bold', bd=3,
+buttonSep = tk.Button(fen, text="Sep", font='Arial 12 bold', bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
-    activebackground='pale turquoise',
-    command=sepSearch)
-buttonSep.pack(in_=top4, side=LEFT, padx=10, pady=10)
+    activebackground='pale turquoise', command=sepSearch)
+buttonSep.pack(in_=top4, side=tk.LEFT, padx=10, pady=10)
 
 def octSearch():
     for path, dirs, files in os.walk('./patient_agenda/'\
@@ -282,18 +274,17 @@ def octSearch():
                         line.replace('}', '')
                         line = lines[i]
                         if line[3:5] == "10":
-                            textBox.insert(INSERT, lines[i-1])
-                            textBox.insert(INSERT, lines[i])
-                            textBox.insert(INSERT, lines[i+1])
-                            textBox.insert(INSERT, lines[i+2])
-                            textBox.insert(INSERT, '\n')
+                            textBox.insert(tk.INSERT, lines[i-1])
+                            textBox.insert(tk.INSERT, lines[i])
+                            textBox.insert(tk.INSERT, lines[i+1])
+                            textBox.insert(tk.INSERT, lines[i+2])
+                            textBox.insert(tk.INSERT, '\n')
                             break
 
-buttonOct = Button(fen, text="Oct", font='Arial 12 bold', bd=3,
+buttonOct = tk.Button(fen, text="Oct", font='Arial 12 bold', bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
-    activebackground='pale turquoise',
-    command=octSearch)
-buttonOct.pack(in_=top4, side=LEFT, padx=10, pady=10)
+    activebackground='pale turquoise', command=octSearch)
+buttonOct.pack(in_=top4, side=tk.LEFT, padx=10, pady=10)
 
 def novSearch():
     for path, dirs, files in os.walk('./patient_agenda/'\
@@ -307,18 +298,17 @@ def novSearch():
                         line.replace('}', '')
                         line = lines[i]
                         if line[3:5] == "11":
-                            textBox.insert(INSERT, lines[i-1])
-                            textBox.insert(INSERT, lines[i])
-                            textBox.insert(INSERT, lines[i+1])
-                            textBox.insert(INSERT, lines[i+2])
-                            textBox.insert(INSERT, '\n')
+                            textBox.insert(tk.INSERT, lines[i-1])
+                            textBox.insert(tk.INSERT, lines[i])
+                            textBox.insert(tk.INSERT, lines[i+1])
+                            textBox.insert(tk.INSERT, lines[i+2])
+                            textBox.insert(tk.INSERT, '\n')
                             break
 
-buttonNov = Button(fen, text="Nov", font='Arial 12 bold', bd=3,
+buttonNov = tk.Button(fen, text="Nov", font='Arial 12 bold', bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
-    activebackground='pale turquoise',
-    command=novSearch)
-buttonNov.pack(in_=top4, side=LEFT, padx=10, pady=10)
+    activebackground='pale turquoise', command=novSearch)
+buttonNov.pack(in_=top4, side=tk.LEFT, padx=10, pady=10)
 
 def decSearch():
     for path, dirs, files in os.walk('./patient_agenda/'\
@@ -332,30 +322,29 @@ def decSearch():
                         line.replace('}', '')
                         line = lines[i]
                         if line[3:5] == "12":
-                            textBox.insert(INSERT, lines[i-1])
-                            textBox.insert(INSERT, lines[i])
-                            textBox.insert(INSERT, lines[i+1])
-                            textBox.insert(INSERT, lines[i+2])
-                            textBox.insert(INSERT, '\n')
+                            textBox.insert(tk.INSERT, lines[i-1])
+                            textBox.insert(tk.INSERT, lines[i])
+                            textBox.insert(tk.INSERT, lines[i+1])
+                            textBox.insert(tk.INSERT, lines[i+2])
+                            textBox.insert(tk.INSERT, '\n')
                             break
 
-buttonDec = Button(fen, text="Dec", font='Arial 12 bold', bd=3,
+buttonDec = tk.Button(fen, text="Dec", font='Arial 12 bold', bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
-    activebackground='pale turquoise',
-    command=decSearch)
-buttonDec.pack(in_=top4, side=LEFT, padx=10, pady=10)
+    activebackground='pale turquoise', command=decSearch)
+buttonDec.pack(in_=top4, side=tk.LEFT, padx=10, pady=10)
 
 def clearTextbox():
     textBox.delete('0.0', "end-1c")
 
-buttonClear = Button(fen, text="Clear", font='Arial 12 bold', 
+buttonClear = tk.Button(fen, text="Clear", font='Arial 12 bold', 
     width=50, bd=3, fg='white', bg='RoyalBlue3',
     highlightbackground='light sky blue',
-    activebackground='pale turquoise', command = clearTextbox)
+    activebackground='pale turquoise', command=clearTextbox)
 buttonClear.pack(in_=top5, padx=10, pady=10)
 
 def msgBox():
-    messagebox.showwarning('WARNING',
+    tk.messagebox.showwarning('WARNING',
         'Error during function call for : ' + line1)
 
 def importFilesFromDir():
@@ -367,12 +356,12 @@ def importFilesFromDir():
             for li in content:
                 li.replace('{', '')
                 li.replace('}', '')
-                textBox.insert(END, li)
+                textBox.insert(tk.END, li)
 
 try:
     importFilesFromDir()
 except OSError as io_err:
-    print("+ Error for calling function !")
+    print("[!] Error for calling function !")
     print(io_err)
     msgBox()
 
