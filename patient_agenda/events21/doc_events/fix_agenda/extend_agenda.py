7a56205d85ef9d0baacde5f8319f3bd605041edd
TLRKiliann <philogenie@protonmail.com>
2021-10-26 16:05:26 +0200

    agenda on work

--- patient_agenda/events21/doc_events/fix_agenda/extend_agenda.py
+++ patient_agenda/events21/doc_events/fix_agenda/extend_agenda.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-import tkinter
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 import os
 import subprocess
@@ -11,61 +10,61 @@ import time
 import shutil
 
 
-fen = Tk()
+fen = tk.Tk()
 fen.title("Time-Track")
 fen.configure(background='DodgerBlue2')
+fen.resizable(False, False)
 
-# To place side by side labelo + entrylab
-top = Frame(fen, bg='DodgerBlue2')
-top2 = Frame(fen, bg='SteelBlue2')
-bottom = Frame(fen, bg='DodgerBlue2')
-top.pack(side=TOP)
-top2.pack(side=TOP)
-bottom.pack(side=BOTTOM, fill=BOTH, expand=YES)
+top = tk.Frame(fen, bg='DodgerBlue2')
+top2 = tk.Frame(fen, bg='SteelBlue2')
+bottom = tk.Frame(fen, bg='DodgerBlue2')
+top.pack(side=tk.TOP)
+top2.pack(side=tk.TOP)
+bottom.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=tk.YES)
 
-labelo = Label(fen, text="Agenda",
+labelo = tk.Label(fen, text="Agenda",
     font='Arial 18 bold',
     fg='white', bg='DodgerBlue2')
-labelo.pack(in_=top, side=LEFT, pady=10)
+labelo.pack(in_=top, side=tk.LEFT, pady=10)
 
 with open('./newpatient/entryfile21.txt', 'r') as filename:
     line1 = filename.readline()
 
-textname = StringVar()
-entryName = Entry(fen, textvariable=textname, width=20, 
+textname = tk.StringVar()
+entryName = tk.Entry(fen, textvariable=textname, width=20, 
     highlightbackground='grey', bd=3)
 textname.set(line1[:-1])
-entryName.pack(in_=top, side=LEFT, padx=10, pady=5)
+entryName.pack(in_=top, side=tk.LEFT, padx=10, pady=5)
 
-labelhour = Label(fen, text="Appointment time : ",
+labelhour = tk.Label(fen, text="Appointment time : ",
     font='Arial 14 bold', fg='white', bg='DodgerBlue2')
-labelhour.pack(in_=top2, side=LEFT, padx=10, pady=10)
+labelhour.pack(in_=top2, side=tk.LEFT, padx=10, pady=10)
 
 def callRefresh():
-    textBox.delete('2.13', END)
+    textBox.delete('2.13', tk.END)
     textBox.update()
     textBox.insert('2.14', entrimehour.get() + \
         ' <--- RDV --- fixed !\n')
 
-hourentr = IntVar()
-entrimehour = Entry(fen, textvariable=hourentr, width=5, 
+hourentr = tk.IntVar()
+entrimehour = tk.Entry(fen, textvariable=hourentr, width=5, 
     highlightbackground='grey', bd=3)
 hourentr.set("00:00")
-entrimehour.pack(in_=top2, side=LEFT, pady=10)
+entrimehour.pack(in_=top2, side=tk.LEFT, pady=10)
 
-buttonhour = Button(fen, text="Enter", width=8, bd=3,
+buttonhour = tk.Button(fen, text="Enter", width=8, bd=3,
     fg="white", bg="RoyalBlue3",
     highlightbackground='light sky blue',
     activebackground='pale turquoise', command=callRefresh)
-buttonhour.pack(in_=top2, side=LEFT, padx=10, pady=10)
+buttonhour.pack(in_=top2, side=tk.LEFT, padx=10, pady=10)
 
 def importationFile(fichier):
     file = open(fichier, 'r')
     content = file.readlines()
     file.close()
     for li in content:
-        textBox.insert(END, li)
-        textBox.insert(END, '\n')
+        textBox.insert(tk.END, li)
+        textBox.insert(tk.END, '\n')
         textBox.delete('2.0')
         textBox.delete('end-3c')
         textBox.insert('2.14', 'Change time and press enter !')
@@ -152,21 +151,21 @@ def retrieve_input():
         print("[Upload] './Backup/Files21' uploaded !")
     else:
         print("[!] No file to upload !")
-        messagebox.showerror("Error", "./Backup/Files21 not uploaded...")
+        tk.messagebox.showerror("Error", "./Backup/Files21 not uploaded...")
 
 def messFromSafeButt():
     """
         To save data when user
         click button save.
     """
-    MsgBox = messagebox.askquestion("Confirm","Are you sure ?\n"
+    MsgBox = tk.messagebox.askquestion("Confirm","Are you sure ?\n"
         "It will save all data !")
     if MsgBox == 'yes':
         retrieve_input()
-        textBox.insert(INSERT, "\n---Data saved !---")
+        textBox.insert(tk.INSERT, "\n---Data saved !---")
         print("[+] Data saved !")
     else:
-        textBox.insert(INSERT, "\n---Nothing has been saved !---")
+        textBox.insert(tk.INSERT, "\n---Nothing has been saved !---")
         print("[!] Nothing has been saved !")
 
 def readerFile():
@@ -184,9 +183,9 @@ def changeText():
     subprocess.run('./patient_agenda/events21/doc_events/'\
         'fix_agenda/main.py', check=True)
 
-textBox = Text(fen, height=15, width=60, font=18)
-textBox.insert(INSERT, "Fixed on : ")
-textBox.insert(END, time.strftime("%d/%m/%Y, %H:%M:%S") + ' :\n')
+textBox = tk.Text(fen, height=15, width=60, font=18)
+textBox.insert(tk.INSERT, "Fixed on : ")
+textBox.insert(tk.END, time.strftime("%d/%m/%Y, %H:%M:%S") + ' :\n')
 textBox.pack(in_=bottom, padx=30, pady=10)
 
 try:
@@ -199,24 +198,24 @@ except FileNotFoundError as nf_file:
     print("[!] File 'patient_value.json' does not exist !")
     print(nf_file)
 
-buttonSave = Button(fen, text="Save", width=8, bd=3,
+buttonSave = tk.Button(fen, text="Save", width=8, bd=3,
     fg='yellow', bg='RoyalBlue3',activebackground='pale turquoise',
     highlightbackground='light sky blue', command=messFromSafeButt)
-buttonSave.pack(in_=bottom, side=LEFT, padx=10, pady=10)
+buttonSave.pack(in_=bottom, side=tk.LEFT, padx=10, pady=10)
 
-buttonRead = Button(fen, text="Read", width=8, bd=3,
+buttonRead = tk.Button(fen, text="Read", width=8, bd=3,
     fg='cyan', bg='RoyalBlue3', activebackground='pale turquoise',
     highlightbackground='light sky blue', command=readerFile)
-buttonRead.pack(in_=bottom, side=LEFT, padx=10, pady=10)
+buttonRead.pack(in_=bottom, side=tk.LEFT, padx=10, pady=10)
 
-buttonDel = Button(fen, text="Modify RDV", width=10, bd=3,
+buttonDel = tk.Button(fen, text="Modify RDV", width=10, bd=3,
     fg='cyan', bg='RoyalBlue3', highlightbackground='light sky blue',
     activebackground='pale turquoise', command=changeText)
-buttonDel.pack(in_=bottom, side=LEFT, padx=10, pady=10)
+buttonDel.pack(in_=bottom, side=tk.LEFT, padx=10, pady=10)
 
-buttonClose = Button(fen, text="Quit", width=8, bd=3,
+buttonClose = tk.Button(fen, text="Quit", width=8, bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
     activebackground='pale turquoise', command=quit)
-buttonClose.pack(in_=bottom, side=RIGHT, padx=10, pady=10)
+buttonClose.pack(in_=bottom, side=tk.RIGHT, padx=10, pady=10)
 
 fen.mainloop()
