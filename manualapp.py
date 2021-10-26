86a69b6f71195d8cf33b5f12df8f77258c784b38
TLRKiliann <philogenie@protonmail.com>
2021-10-27 01:05:10 +0200

    tutorial improved

--- manualapp.py
+++ manualapp.py
@@ -14,7 +14,7 @@ def instalpy(self):
     self.effacer()
     self.delScroll()
 
-    self.photo = tk.PhotoImage(file='./syno_gif/minipy3.png')
+    self.photo = tk.PhotoImage(file='./syno_gif/2b_bg.png')
 
     self.item = tk.Label(self.can, image=self.photo, bg='black')
     self.item.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
@@ -23,57 +23,87 @@ def instalpy(self):
         highlightbackground='black', width=92, height=30, font=("Times New Roman", 14), bd=0)
 
     self.text_area.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
-    self.text_area.insert(tk.END,"\n - MAPAPP -\n\n"
+    self.text_area.insert(tk.END,"\n --------------------\n"
+        " |- TUTORIAL -|\n"
+        " --------------------\n"
 
-        "\n Usefull functionalities :\n"
-        " ----------------------------\n"
-        " ---> Backup is scheduled every 5 days and each month for Vital Parameters and BMI\n"
-        " ---> Display if a treatment or reserve ends the next day and print a stop into tabs\n"
-        " You need to write name of 'Treatment' to realy stop it !\n"
-        " ---> Agenda is verified every day and a reminder appear to show you" \
-        " if an appointment is fixed for tomorrow\n\n"
+        "\n [+] Usefull functionalities :\n"
+        " ----------------------------------\n"
+        " [*] Backup is scheduled every 5 days and each month for Vital Parameters and BMI\n"
+        " [*] Display if a treatment or reserve ends the next day and print a stop into tabs\n"
+        " [!] You need to write name of 'MEDICATION' to realy stop it [!]\n"
+        " [*] Agenda is verified every day and a reminder appear to show you"\
+        " if an appointment is fixed for tomorrow\n"
+        " [*] A programmable alarm is available as long as the application remains open.\n\n"
 
-        " How to use heal-track :\n\n"
+        " How to use Time-Track :\n\n"
         " To launch app :\n"
         " > python3 heal_track.py\n\n"
 
-        " To make a patient entry : \n"
+        " [+] Enter a patient in the application : \n"
+        " ----------------------------------------------\n"
 
-        " Entry or Add patient (read below) ---> Allergy + Intolerance --->" \
-        " 14 Needs ---> Care and Monitoring :\n"
-        " -----------------------------------------------------------------"
-        " ---------------------------------------------------------\n"
-        " Use 'Entry' button to enter for first time new patient. Use 'Add " \
-        " patient' once time all patients were enter \n"
-        " (button to replace a patient who's left with delete button).\n"
-        " Once time, patient had added use 'Update' button to enter an allergy " \
+        " [-->] Entry or Add patient (read below) --> Allergy --> Intolerance -->" \
+        " 14 Needs --> Care and Monitoring :\n\n"
+
+        " Use <New Entry> button to enter for first time new patient. Use <Add " \
+        " patient> once time all patients were\n"
+        " <Enter> ......... \n"
+        " (button to replace a patient who's left with <Delete> button).\n"
+        " Once time, patient had added use <Update> button to enter an allergy " \
         " if none, write 'none' and press enter !\n"
-        " You can also use 'Intolerance' in the Menu Bar to complete 'allergy'.\n"
-        " After it, Care and Monitoring is available only if you have entered one " \
+        " You can also use <Intolerance> in the Menu Bar to complete <Allergy>>.\n"
+        " After it, <Care and Monitoring> is available only if you have entered one " \
         " or more needs of patient.\n\n"
-        " 1 ---> Press <add patient> button.\n"
-        " 2 ---> Write the number by which it will be classified in the database.\n"
-        " 2 ---> Write data of patient and press <Enter>.\n"
-        " 3 ---> Use <Refresh> button and look at if name has changed.\n\n"
+        " [1] ---> Press <add patient> button.\n"
+        " [2] ---> Write the number by which it will be classified in the database.\n"
+        " [3] ---> Write data of patient and press <Enter>.\n"
+        " [4] ---> Use <Refresh> button and look at if name has changed.\n\n"
         " Update for Allergia - Intolerance - Diagnosis :\n"
-        " 4 ---> Use <Update> button to write allergia into DB\n"
-        " 5 ---> Click on <Intolerance All> (in menubar) and save data.\n\n"
-        "\nA revoir le fonctionnement !!!\n\n"
-        " To verify :\n"
-        " * ---> Click on <DataBase>.\n"
-        " * ---> Click on <14 Needs>.\n"
-        " * ---> Click on <Care and monitoring>.\n\n"
+        " [5] ---> Use <Update> button to write allergia into DB\n"
+        " [6] ---> Click on <Intolerance> (in menubar) and save data.\n\n"
 
-        " Care and monitoring retrieve all data from :\n"
-        " ----------------------------------------------------\n"
-        " + 14 Needs (V. Henderson)\n"
-        " + Treatments (show Treatment(s) and Reserve(s) stopped).\n"
-        " + Labo (Neuroleptic dosages on which the results can be added in care and monitoring).\n"
-        " + Stix (which the results can be added in care and monitoring too).\n\n"
+        " To be verified :\n"
+        " [-->] Click on <DataBase> for new entry and when it's empty.\n"
+        " [-->] Click on <14 Needs>.\n"
+        " [-->] Click on <Care and monitoring>.\n\n"
 
-        " Good point of view over all files :\n"
-        " ----------------------------------------\n"
-        " + Click on 'Global' button and search file you're interested by.\n"
+        " [+] Alarm :\n"
+        "----------------\n"
+        " [*] Program the alarm by entering a comment. It will work as long as the application\n"
+        " remains open.\n\n"
+
+        " [+] Agenda :\n"
+        "----------------\n"
+        " [1] Press <choose a date> date button and press <fix appointment> to follow step.\n"
+        " [2] You have the possibility to fix an appointment after.\n"
+        " [*] An alrm will ring one day before appointment.\n"
+        " [*] You can change in everytime the clock or the data of appointment.\n"
+        " [*] You can see all RDV wth a reader.\n\n"
+
+        " [+] 14 Needs :\n"
+        "------------------\n"
+        " [*] After having record all data about a patient you can click on the need.\n"
+        " [*] The record is report to care and monitoring. You have to save it.\n\n"
+
+        " [+] Care and monitoring retrieve all data from :\n"
+        " --------------------------------------------------------\n"
+        " [*] 14 Needs (V. Henderson)\n"
+        " [*] Treatments (show Treatment(s) and Reserve(s) stopped).\n"
+        " [*] Labo (Neuroleptic dosages on which the results can be added in care and monitoring).\n"
+        " [*] Stix (which the results can be added in care and monitoring too).\n\n"
+
+        " [+] Vital Parameters :\n"
+        " -------------------------\n"
+        " [*] The vp allows you to enter parameters such as blood pressure, pulse, respiratory rate, \n"
+        " oxygen saturation, temperature, blood sugar, pain and to display them in the form of graphs.\n"
+        " [*] The read button allows you to read the data in a file.\n"
+        " [*] A text box is included in the frame for data insertion. It's nicer and especially the data\n"
+        " may appear more easily.\n\n"
+
+        " [+] Good point of view over all files :\n"
+        " --------------------------------------------\n"
+        " [*] Click on <Global> button and search file you're interested by.\n"
         " You can put all files you want in this folder, but it will not be uploaded to server.\n"
         " For that it's better to upload it with scp command, such as : \n"
         " (with python3 command :)\n"
@@ -82,7 +112,7 @@ def instalpy(self):
         " (or with shell command :)\n"
         " $ scp ./Backup/FilesX/yourfile.xyz pi@192.168.18.12:~/tt_doc/doc_txtX/FilesX/yourfile.xyz\n"
 
-        " + Click on 'EventBox' for seeing previously about yesterday news and backup will"\
+        " [*] Click on <EventBox> for seeing previously about yesterday news and backup will"\
         " be launched every 5 days.\n"
 
         "\n !!! WARNING !!! : Don't press 'delete patient' before informing administrator, \n"\
