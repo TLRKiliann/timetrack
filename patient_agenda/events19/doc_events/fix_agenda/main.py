be6a23fcdad36e9ef4552d259223ef9420999fe6
TLRKiliann <philogenie@protonmail.com>
2021-10-26 14:43:58 +0200

    agenda on work

--- patient_agenda/events19/doc_events/fix_agenda/main.py
+++ patient_agenda/events19/doc_events/fix_agenda/main.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-import tkinter
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 import json
 import time
@@ -12,43 +11,45 @@ import subprocess
 from itertools import *
 
 
-gui = Tk()
+gui = tk.Tk()
 gui.title("Time-Track")
 gui.configure(background='DodgerBlue2')
+gui.resizable(False, False)
 
-top = Frame(gui, bg='DodgerBlue2')
-top2 = Frame(gui, bg='SteelBlue2')
-bottom = Frame(gui, bg='DodgerBlue2')
-top.pack(side=TOP)
-top2.pack(side=TOP)
-bottom.pack(side=BOTTOM, fill=BOTH, expand=YES)
+top = tk.Frame(gui, bg='DodgerBlue2')
+top2 = tk.Frame(gui, bg='SteelBlue2')
+bottom = tk.Frame(gui, bg='DodgerBlue2')
+top.pack(side=tk.TOP)
+top2.pack(side=tk.TOP)
+bottom.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=tk.YES)
 
-labelTit = Label(gui, text="Save changes !", font=("Arial 16 bold"),
+labelTit = tk.Label(gui, text="Save changes !", font=("Arial 16 bold"),
     fg='white', bg='DodgerBlue2')
-labelTit.pack(in_=top, side=LEFT, pady=10)
+labelTit.pack(in_=top, side=tk.LEFT, pady=10)
 
 with open('./newpatient/entryfile19.txt', 'r') as filename:
     line1 = filename.readline()
 
-textname = StringVar()
-entryName = Entry(gui, textvariable=textname, width=20, 
+textname = tk.StringVar()
+entryName = tk.Entry(gui, textvariable=textname, width=20, 
     highlightbackground='grey', bd=3)
 textname.set(line1[:-1])
-entryName.pack(in_=top, side=LEFT, padx=10, pady=10)
+entryName.pack(in_=top, side=tk.LEFT, padx=10, pady=10)
 
-labelDate = Label(gui, text='Search date to modify : ',
+labelDate = tk.Label(gui, text='Search date to modify : ',
     font='Arial 14 bold', fg='white', bg='DodgerBlue2')
-labelDate.pack(in_=top2, side=LEFT, padx=10, pady=10)
+labelDate.pack(in_=top2, side=tk.LEFT, padx=10, pady=10)
 
 def searchExpress():
     """
         To read multiples files in a directory
     """
+    hour_var = reachHour.get()
+    oclock = reachHour.get()
+    regexpi_var = reachDate.get()
+    mot = reachDate.get()
+
     try:
-        hour_var = reachHour.get()
-        oclock = reachHour.get()
-        regexpi_var = reachDate.get()
-        mot = reachDate.get()
         for path, dirs, files in os.walk('./patient_agenda/events19/'\
             'doc_events/fix_agenda/agenda_saved/'):
             for file in files:
@@ -60,35 +61,35 @@ def searchExpress():
                             if mot in line:
                                 if oclock in line:
                                     print("Print line !")
-                                    textBox.insert(INSERT, lines[i-1])
-                                    textBox.insert(INSERT, lines[i])
-                                    textBox.insert(INSERT, lines[i+1])
-                                    textBox.insert(INSERT, lines[i+2])
-                                    textBox.insert(INSERT, '\n')
+                                    textBox.insert(tk.INSERT, lines[i-1])
+                                    textBox.insert(tk.INSERT, lines[i])
+                                    textBox.insert(tk.INSERT, lines[i+1])
+                                    textBox.insert(tk.INSERT, lines[i+2])
+                                    textBox.insert(tk.INSERT, '\n')
                                     break
     except IndexError as ind_err:
-        print("+ Index out of range", ind_err)
+        print("[!] Index out of range", ind_err)
 
-regexpi_var = StringVar()
-reachDate = Entry(gui, textvariable=regexpi_var, width=10, 
+regexpi_var = tk.StringVar()
+reachDate = tk.Entry(gui, textvariable=regexpi_var, width=10, 
     highlightbackground='grey', bd=3)
-regexpi_var.set("00/00/2021")
-reachDate.pack(in_=top2, side=LEFT, pady=10)
+regexpi_var.set(time.strftime("%d/%m/%Y"))
+reachDate.pack(in_=top2, side=tk.LEFT, pady=10)
 
-labelhour = Label(gui, text='Hour : ',
+labelhour = tk.Label(gui, text='Hour : ',
     font='Arial 14 bold', fg='white', bg='DodgerBlue2')
-labelhour.pack(in_=top2, side=LEFT, padx=10, pady=10)
+labelhour.pack(in_=top2, side=tk.LEFT, padx=10, pady=10)
 
-hour_var = IntVar()
-reachHour = Entry(gui, textvariable=hour_var, width=5,
+hour_var = tk.IntVar()
+reachHour = tk.Entry(gui, textvariable=hour_var, width=5,
     highlightbackground='grey', bd=3)
 hour_var.set('00:00')
-reachHour.pack(in_=top2, side=LEFT, pady=10)
+reachHour.pack(in_=top2, side=tk.LEFT, pady=10)
 
-buttonSearch = Button(gui, text='Search', width=8, bd=3,
+buttonSearch = tk.Button(gui, text='Search', width=8, bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
     activebackground='pale turquoise', command=searchExpress)
-buttonSearch.pack(in_=top2, side=LEFT, padx=10, pady=10)
+buttonSearch.pack(in_=top2, side=tk.LEFT, padx=10, pady=10)
 
 def save_input():
     """
@@ -101,6 +102,7 @@ def save_input():
     oclock = reachHour.get()
     regexpi_var = reachDate.get()
     magicword = reachDate.get()
+
     for path, dirs, files in os.walk('./patient_agenda/events19/'\
         'doc_events/fix_agenda/agenda_saved/'):
         for file in files:
@@ -109,19 +111,19 @@ def save_input():
                 for k in line:
                     noway = "Fixed on :"
                     if line[0:10] == noway:
-                        print("+ There is noway : ")
+                        print("[!] No magicword : ")
                         print(line[0:10])
                     elif magicword in line:
                         if oclock in line:
-                            print("+ It is magicword : ")
+                            print("[+] It is magicword : ")
                             print(line[0:10])
                             print(line[13:21])
                             write_f = open(os.path.join(path, file), 'w')
                             write_f.writelines(textBox.get("0.0", "end-1c"))
-                            print("Modification finish")
+                            print("[+] Modification finish")
                             break
                     else:
-                        print("None file has been writted")
+                        print("[!] None file has been writted")
                         break
 
     secproc = subprocess.run(["scp", "-r",
@@ -130,21 +132,21 @@ def save_input():
         stderr=subprocess.PIPE)
     print("Result SCP transfert : %s" % repr(secproc.stderr))
     if secproc.stderr == b'':
-        print("+ './Backup/Files19' uploaded !")
+        print("[Upload] './Backup/Files19' uploaded !")
     else:
-        print("+ No file to upload !")
-        messagebox.showerror("Error", "./Backup/Files19 not uploaded...")
+        print("[!] No file to upload !")
+        tk.messagebox.showerror("Error", "./Backup/Files19 not uploaded...")
 
 def messFromSafeButt():
-    MsgBox = messagebox.askquestion("Confirm","Are you sure ?\n"
+    MsgBox = tk.messagebox.askquestion("Confirm","Are you sure ?\n"
         "It will save all data !")
     if MsgBox == 'yes':
         save_input()
-        textBox.insert(INSERT, "\n---Data saved !---")
-        print("+ Data saved !")
+        textBox.insert(tk.INSERT, "\n---Data saved !---")
+        print("[+] Data saved !")
     else:
-        textBox.insert(INSERT, "\n---Nothing has been saved !---")
-        print("+ Nothing has been saved !")
+        textBox.insert(tk.INSERT, "\n---Nothing has been saved !---")
+        print("[!] Nothing has been saved !")
 
 def modifList():
     """
@@ -156,27 +158,27 @@ def modifList():
 def deleteTextbox():
     textBox.delete('0.0', "end-1c")
 
-textBox = Text(gui, height=15, width=60, font=18)
+textBox = tk.Text(gui, height=15, width=60, font=18)
 textBox.pack(in_=bottom, padx=30, pady=10)
 
-buttonSave = Button(gui, text="Save", width=8, bd=3,
+buttonSave = tk.Button(gui, text="Save", width=8, bd=3,
     fg='yellow', bg='RoyalBlue3', highlightbackground='light sky blue',
     activebackground='pale turquoise', command = messFromSafeButt)
-buttonSave.pack(in_=bottom, side=LEFT, padx=10, pady=10)
+buttonSave.pack(in_=bottom, side=tk.LEFT, padx=10, pady=10)
 
-buttonModif = Button(gui, text="Read", width=8, bd=3,
+buttonModif = tk.Button(gui, text="Read", width=8, bd=3,
     fg='cyan', bg='RoyalBlue3', highlightbackground='light sky blue',
     activebackground='pale turquoise', command = modifList)
-buttonModif.pack(in_=bottom, side=LEFT, padx=10, pady=10)
+buttonModif.pack(in_=bottom, side=tk.LEFT, padx=10, pady=10)
 
-buttonDelete = Button(gui, text="Clear", width=8, bd=3,
+buttonDelete = tk.Button(gui, text="Clear", width=8, bd=3,
     fg='cyan', bg='RoyalBlue3', highlightbackground='light sky blue',
     activebackground='pale turquoise', command = deleteTextbox)
-buttonDelete.pack(in_=bottom, side=LEFT, padx=10, pady=10)
+buttonDelete.pack(in_=bottom, side=tk.LEFT, padx=10, pady=10)
 
-buttonQuit = Button(gui, text='Quit', width=8, bd=3,
+buttonQuit = tk.Button(gui, text='Quit', width=8, bd=3,
     fg='white', bg='RoyalBlue3', highlightbackground='light sky blue',
     activebackground='pale turquoise', command=quit)
-buttonQuit.pack(in_=bottom, side=RIGHT, padx=10, pady=10)
+buttonQuit.pack(in_=bottom, side=tk.RIGHT, padx=10, pady=10)
 
 gui.mainloop()
