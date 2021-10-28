99d01b97f3859509e81e09c49535868268a98484
TLRKiliann <philogenie@protonmail.com>
2021-10-28 12:23:54 +0200

    threadings improved for progressbar

--- heal_track.py
+++ heal_track.py
@@ -33,9 +33,9 @@ from patcaps import callResident
 from backapp import *
 from Backup.backupfile import dataBackToSave
 
-from need.needownload.refdlneed import needload1
+from need.needownload.refdlneed import *
 
-from need.needownload.download import *
+#from need.needownload.download import *
 from param.backup_month import paramBackToSave
 from calBmi.bmi_backup import bmiBackToSave
 from vmed.medload import *
