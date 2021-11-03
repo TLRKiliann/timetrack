e2539bf8de90147f88c565c0171a766416f11d7d
TLRKiliann <philogenie@protonmail.com>
2021-11-03 01:16:40 +0100

    contact on work

---
+++ contact/conpact9/writerfiles/writefam9.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+import tkinter as tk
+import os
+
+
+def recorderFam(self):
+    """
+        Display origin
+    """
+    try:
+        if os.path.getsize('./contact/conpact9/famycontact9.txt'):
+            print("[+] Ok, famycontact9.txt exist (t2)")
+    except FileNotFoundError as err_fnf:
+        print("[!] No file famycontact9.txt exist", err_fnf)
+        with open('./contact/conpact9/famycontact9.txt', 'w') as testf:
+            print("[+] File famycontact9.txt created !")
+
+    try:
+        with open('./contact/conpact9/famycontact9.txt', 'w') as iofile:
+            iofile.write(self.namentry.get())
+            iofile.write("\n" + self.phonentry.get())
+            iofile.write("\n" + self.mobilentry.get())
+            iofile.write("\n" + self.addrentry.get())
+            iofile.write("\n" + self.cityentry.get())
+            iofile.write("\n" + self.entrymail.get())
+    except FileNotFoundError as fn:
+        print("[!] File famycontact9.txt not found !", fn)
+
+    try:
+        if os.path.getsize('./contact/conpact9/finalfam9.txt'):
+            os.remove('./contact/conpact9/finalfam9.txt')
+    except FileNotFoundError as err_termin:
+        print("[!] finalfam9.txt not found !(t2)", err_termin)
+        with open('./contact/conpact9/finalfam9.txt', 'a+'):
+            print("[+] finalfam9.txt exist!")
+
+    try:
+        with open('./contact/conpact9/finalfam9.txt', 'w') as terminfile:
+            terminfile.write("Name : " + self.namentry.get())
+            terminfile.write("\nPhone : " + self.phonentry.get())
+            terminfile.write("\nPhone : " + self.mobilentry.get())
+            terminfile.write("\nStreet : " + self.addrentry.get())
+            terminfile.write("\nCity : " + self.cityentry.get())
+            terminfile.write("\ne-mail : " + self.entrymail.get())
+    except FileNotFoundError as err2_final:
+        print("[!] finalfam9.txt not created (t2)", err2_final)
