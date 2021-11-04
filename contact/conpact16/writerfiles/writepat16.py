f348119a59e9b58b952d01e2ff267a7fcf6adb61
TLRKiliann <philogenie@protonmail.com>
2021-11-04 18:15:40 +0100

    contact on work

---
+++ contact/conpact16/writerfiles/writepat16.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+import tkinter as tk
+import os
+
+
+def recorderData(self, birthvar):
+    """
+        Display origin
+    """
+    try:
+        with open('./contact/conpact16/contact16.txt', 'w') as iofile:
+            iofile.write(self.namentry.get())
+            iofile.write("\n" + birthvar)
+            iofile.write("\n" + self.nativaentry.get())
+            iofile.write("\n" + self.phonentry.get())
+            iofile.write("\n" + self.addrentry.get())
+            iofile.write("\n" + self.cityentry.get())
+            iofile.write("\n" + self.entrymail.get())
+            iofile.write("\n" + self.entryassu.get())
+            iofile.write("\n" + self.entrypolicy.get())
+            iofile.write("\n" + self.entrycivil.get())
+            iofile.write("\n" + self.entryconfess.get())
+    except FileNotFoundError as fn:
+        print("[!] File not found !", fn)
+
+    try:
+        if os.path.getsize('./contact/conpact16/finalfile16.txt'):
+            os.remove('./contact/conpact16/finalfile16.txt')
+    except FileNotFoundError as err_termin:
+        print("[!] finalfile16.txt not found !(Error_3)", err_termin)
+        with open('./contact/conpact16/finalfile16.txt', 'a+'):
+            print("[+] File finalfile16.txt exist!")
+
+    try:
+        with open('./contact/conpact16/finalfile16.txt', 'w') as terminfile:
+            terminfile.write("Patient name : " + self.namentry.get())
+            terminfile.write("\nBirthdate : " + birthvar)
+            terminfile.write("\nNative : " + self.nativaentry.get())
+            terminfile.write("\nPhone : " + self.phonentry.get())
+            terminfile.write("\nStreet : " + self.addrentry.get())
+            terminfile.write("\nCity : " + self.cityentry.get())
+            terminfile.write("\ne-mail : " + self.entrymail.get())
+            terminfile.write("\nInsurance : " + self.entryassu.get())
+            terminfile.write("\nPolicy : " + self.entrypolicy.get())
+            terminfile.write("\nCivil status : " + self.entrycivil.get())
+            terminfile.write("\nConfession : " + self.entryconfess.get())
+    except FileNotFoundError as err2_final:
+        print("[!] finalfile16.txt not created (Error_4)", err2_final)
